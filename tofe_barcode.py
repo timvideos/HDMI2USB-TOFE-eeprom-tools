6eecffd568a34cd27f385a53d10fbd298c6addf9
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-24 16:34:16 +1100

    Removing the Opsis EEPROM stuff.

--- tofe_barcode.py
+++ tofe_barcode.py
@@ -1,6 +1,3 @@
-
-
-
 import subprocess
 import barcode
 from barcode.writer import ImageWriter
@@ -20,12 +17,3 @@ params = {'module_height': 19.00, 'module_width': 0.28350000000000000, 'font_siz
 l.save('barcode-tofe-lowspeedio', params)
 print(840, 255)
 subprocess.call("file barcode-tofe-lowspeedio.png", shell=True)
-
-# Needs to be 3.5 inch by 0.500 inch at 300dpi
-# 3.500 * 300 == 1050.0
-# 0.500 * 300 ==  150.0
-params = {'module_height': 10.10, 'module_width': 0.50200000000000000, 'font_size': 15, 'text_distance': -2, 'human': ' '}
-o = barcode.get('Code128', "opsis.h2u.tv", writer=ImageWriter())
-o.save('barcode-opsis', params)
-print(1050.0, 150.0)
-subprocess.call("file barcode-opsis.png", shell=True)
